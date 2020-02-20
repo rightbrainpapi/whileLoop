69bc07bb94e21bad2d54a7ef076ef690a72e7e90
rightbrainpapi <dahuemaen@gmail.com>
2020-02-20 03:15:21 -0500

    using if  else statement to make elephant run

--- main.js
+++ main.js
@@ -26,6 +26,40 @@ function elephantRuns() {
 
 
 
+
+// function elephantMoveDown() {
+//   // change the still elephant to gif
+//   var elem = document.getElementById("myElephant");   
+//   var posL = elem.style.left;
+//   var posR = elem.style.right;
+//   var id = setInterval(frame, 10);
+//   function frame() {
+
+//     if (posL == 225) {
+//       clearInterval(id);
+//       console.log("cleared")
+//     } else {
+//       posL++; 
+//       elem.style.top = posL + 'px';
+//       // console.log(posR);
+//       console.log(posL);
+
+//       //elem.style.bottom = pos + 'px'; 
+//     }
+  
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
 // This is the click function 
 function toggleMouse(){
   // makes mouse visible
@@ -33,20 +67,38 @@ function toggleMouse(){
 
   // If else Statement to controll whether mouse is in Sancuary
   if (ourMouseStyle.display=="none"){
-
-    // we want the moving 
+    // we want the show mouse 
     ourMouseStyle.display="block";
     isMouseVisible = true;
+    // !isMouseVisible ? elephantMoveDown() : console.log("not visi") ;
     }
     else {
-      ourMouseStyle.display="none";
+      ourMouseStyle.display="none"; //this is resetting not a condition
       isMouseVisible = false;
     }
 
-    isMouseVisible ? elephantRuns() : console.log('there is no mouse');
 
+
+
+      // If else Statement to controll whether mouse is in Sancuary
+  if (isMouseVisible){
+    // If Mouse is visible
+    elephantRuns()
+    }
+    else {
+      console.log('there is no mouse')
+    }
+
+
+
+
+
+    // isMouseVisible ? elephantRuns() : console.log('there is no mouse');
+    
+    
+    
 }
-toggleMouse();
+// toggleMouse();
 
 
 
